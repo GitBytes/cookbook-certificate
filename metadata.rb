93f6808751bc9770aa9b53d6a0649c6b3b9d143a
I Smith <ian.smith@pnnl.gov>
2016-10-10 15:20:50 -0700

    berks isn't liking 'supports', will investigate further, moving on for now

--- metadata.rb
+++ metadata.rb
@@ -9,13 +9,3 @@ issues_url 'https://github.com/atomic-penguin/cookbook-certificate/issues'
 version '2.0.0'
 
 depends 'chef-vault', '~> 2.0'
-
-supports amazon # <- Not tested
-supports centos, '>= 5.11' # Tested
-supports debian, '>= 7.8' # <- Assumed to work
-supports fedora, '>= 22' # Tested
-supports oracle # <- Not tested
-supports redhat, '>= 5.11' # <- Assumed to work
-supports scientific, '>= 5' # <- Assummed to work
-supports smartos # <- Not tested
-supports ubuntu, '>= 10.04' # Tested
